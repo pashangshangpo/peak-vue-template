1f04bc9f48b28a03edda44016b8da66debd9b029
爬上上坡 <pashangshangpo@gmail.com>
2018-11-30 22:13:20 +0800

    删除 Button 组件，添加 Icon 组件

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg :class="className" aria-hidden="true">
+    <use :xlink:href="iconId"></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: String,
+      className: {
+        default: 'icon',
+        type: String
+      }
+    },
+    computed:{
+      iconId(){
+        return `#icon-${this.name}`
+      }
+    }
+  }
+</script>
